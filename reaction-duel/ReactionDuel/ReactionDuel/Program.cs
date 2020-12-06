7368797d41f832c342cc228e574c9fa2f5d13164
Benedict Hazel <bwhazel@gmail.com>
2020-12-06 10:43:22 +0000

    Add Visual Studio solution for Reaction Duel.

---
+++ reaction-duel/ReactionDuel/ReactionDuel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReactionDuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
