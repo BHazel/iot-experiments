e33dcc39c45b664c2636fffc3e9d656cae5cc803
Benedict Hazel <bwhazel@gmail.com>
2021-02-06 15:15:26 +0000

    Add a loop to Quantum Telloport to create drone commands for 2 minutes with 10 second pauses between each.

--- quantum-telloport/QuantumTelloport/QuantumTelloport/Program.cs
+++ quantum-telloport/QuantumTelloport/QuantumTelloport/Program.cs
@@ -1,23 +1,33 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
+using System.Threading;
 using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.Simulation.Simulators;
 using BWHazel.Apps.QuantumTelloport.Control;
 
 const long MinimumDroneDistance = 20;
+const int MovementWaitTime = 10000;
+TimeSpan totalRunTime = new TimeSpan(0, 2, 0);
 
-QuantumSimulator quantumSimulator = new();
-long axis = await DetermineAxis.Run(quantumSimulator);
-long direction = await DetermineDirection.Run(quantumSimulator);
-long distance = await DetermineDistance.Run(quantumSimulator);
-
-long droneDistance = distance + MinimumDroneDistance;
-string droneCommand = (axis, direction) switch
+DateTime startTime = DateTime.Now;
+while (DateTime.Now - startTime <= totalRunTime)
 {
-    (1, 1) => $"forward {droneDistance}",
-    (1, 0) => $"backward {droneDistance}",
-    (0, 1) => $"right {droneDistance}",
-    (0, 0) => $"left {droneDistance}",
-    (_, _) => $"land"
-};
+    WriteLine(DateTime.Now - startTime);
+    QuantumSimulator quantumSimulator = new();
+    long axis = await DetermineAxis.Run(quantumSimulator);
+    long direction = await DetermineDirection.Run(quantumSimulator);
+    long distance = await DetermineDistance.Run(quantumSimulator);
 
-WriteLine(droneCommand);
+    long droneDistance = distance + MinimumDroneDistance;
+    string droneCommand = (axis, direction) switch
+    {
+        (1, 1) => $"forward {droneDistance}",
+        (1, 0) => $"backward {droneDistance}",
+        (0, 1) => $"right {droneDistance}",
+        (0, 0) => $"left {droneDistance}",
+        (_, _) => $"land"
+    };
+
+    WriteLine(droneCommand);
+    Thread.Sleep(MovementWaitTime);
+}
