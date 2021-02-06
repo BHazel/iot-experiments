dcd3aceb421e1eff34dd2e52a0b58cc954b5c6f5
Benedict Hazel <bwhazel@gmail.com>
2021-02-06 10:56:14 +0000

    Add Visual Studio solution for Quantum Telloport.

---
+++ quantum-telloport/QuantumTelloport/QuantumTelloport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BWHazel.Apps.QuantumTelloport
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
