715458081864d23e141065ed560f1529b12f31f0
Benedict Hazel <bwhazel@gmail.com>
2021-02-07 10:35:15 +0000

    Integrate Ryze Tello flight control into Quantum Telloport and update assembly name.

--- quantum-telloport/QuantumTelloport/QuantumTelloport/Program.cs
+++ quantum-telloport/QuantumTelloport/QuantumTelloport/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using static System.Console;
+using System.Net;
 using System.Threading;
 using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.Simulation.Simulators;
 using BWHazel.Apps.QuantumTelloport.Control;
+using TelloCommander.CommandDictionaries;
+using TelloCommander.Commander;
+using TelloCommander.Connections;
 
 const long MinimumDroneDistance = 20;
 const int MovementWaitTime = 10000;
+const string TelloApiVersion = "1.3.0.0";
 TimeSpan totalRunTime = new TimeSpan(0, 2, 0);
+
+CommandDictionary commandDictionary = CommandDictionary.ReadStandardDictionary(TelloApiVersion);
+TelloConnection telloConnection = new(IPAddress.Loopback.ToString(),
+    TelloConnection.DefaultTelloPort,
+    ConnectionType.Simulator
+);
+
+DroneCommander droneCommander = new(telloConnection, commandDictionary);
+droneCommander.Connect();
+droneCommander.RunCommand("takeoff");
+
+CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
+{
+    droneCommander.RunCommand("land");
+};
 
 DateTime startTime = DateTime.Now;
 while (DateTime.Now - startTime <= totalRunTime)
@@ -22,12 +42,16 @@ while (DateTime.Now - startTime <= totalRunTime)
     string droneCommand = (axis, direction) switch
     {
         (1, 1) => $"forward {droneDistance}",
-        (1, 0) => $"backward {droneDistance}",
+        (1, 0) => $"back {droneDistance}",
         (0, 1) => $"right {droneDistance}",
         (0, 0) => $"left {droneDistance}",
         (_, _) => $"land"
     };
 
+    droneCommander.RunCommand(droneCommand);
+
     WriteLine(droneCommand);
     Thread.Sleep(MovementWaitTime);
 }
+
+droneCommander.Disconnect();
